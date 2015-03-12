667c9d5c12e648cd63b63164449623675527305c
Wellington Ribeiro <devwellington@gmail.com>
2015-03-11 23:06:06 -0300

    Fix coverage Class [Option]

---
+++ tests/src/DevWellington/HTML/Components/OptionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DevWellington\HTML\Components;
+
+class OptionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testVerificaSePertenceAInterfaceIOption()
+    {
+        $op = new Option();
+        $this->assertInstanceOf('\DevWellington\HTML\Components\IOption', $op);
+    }
+
+    public function testVerificarSeConsegueAClasseOption()
+    {
+        $op = new Option();
+        $this->assertInstanceOf('\DevWellington\HTML\Components\Option', $op);
+    }
+
+    public function testVerificaSeConsegueInserirAtributosNoComponenteOption()
+    {
+        $op = new Option();
+        $op
+            ->setValue(0)
+            ->setDescription('Opcao 0')
+        ;
+
+        $this->assertEquals(0, $op->getValue());
+        $this->assertEquals('Opcao 0', $op->getDescription());
+
+    }
+} 
